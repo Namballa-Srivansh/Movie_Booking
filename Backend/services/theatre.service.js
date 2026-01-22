34c27a4f8b1b5d06897b260ae154e5e20c38b7d5
Namballa-Srivansh <srivansh90@gmail.com>
2026-01-22 15:02:35 +0530

    Some minor changes

--- Backend/services/theatre.service.js
+++ Backend/services/theatre.service.js
@@ -1,59 +1,65 @@
-const Theatre = require('../models/theatre.model');
+const Theatre = require("../models/theatre.model");
 
 const createTheatre = async (data) => {
-    try {
-        console.log("Received data for theatre creation:", data);
-        // Remove _id if present to ensure new document creation
-        delete data._id;
-        console.log("Data after removing _id:", data);
-        const response = await Theatre.create(data);
-        console.log("Created theatre:", response);
-        return response;
-    } catch (err) {
-        if(err.name ==="ValidationError") {
-            let err = {};
-            Object.keys(err.errors).forEach((key) => {
-                err[key] = err.errors[key].message;
-            });
-            return {err: err, code: 422}
-        }
-        if (err.code === 11000) {
-            return {err: {name: "Theatre with this name already exists"}, code: 409}
-        }
-        console.log("Error in creating theatre", err);
-        throw err;
+  try {
+    console.log("Received data for theatre creation:", data);
+    // Remove _id if present to ensure new document creation
+    delete data._id;
+    console.log("Data after removing _id:", data);
+    const response = await Theatre.create(data);
+    console.log("Created theatre:", response);
+    return response;
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      let err = {};
+      Object.keys(err.errors).forEach((key) => {
+        err[key] = err.errors[key].message;
+      });
+      return { err: err, code: 422 };
     }
-}
+    if (err.code === 11000) {
+      return {
+        err: { name: "Theatre with this name already exists" },
+        code: 409,
+      };
+    }
+    console.log("Error in creating theatre", err);
+    throw err;
+  }
+};
 
-const deleteTheatre = async(id) => {
+const deleteTheatre = async (id) => {
   try {
     const response = await Theatre.findByIdAndDelete(id);
-    if(!response) {
+    if (!response) {
       return {
         err: "Theatre not found",
-        code: 404
-      }
-    }
-    return response;
-  } catch(err) {
-    throw new Error("Error in deleting theatre");
-  }
-}
-
-const updateTheatre = async(id, data) => {
-  try {
-    const response = await Theatre.findByIdAndUpdate(id, data, {new: true, runValidators: true});
-    if(!response) {
-      return {
-        err: "Theatre not found",
-        code: 404
+        code: 404,
       };
     }
     return response;
-  } catch(err) {
+  } catch (err) {
+    throw new Error("Error in deleting theatre");
+  }
+};
+
+const updateTheatre = async (id, data) => {
+  try {
+    const response = await Theatre.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    });
+    if (!response) {
+      return {
+        err: "Theatre not found",
+        code: 404,
+      };
+    }
+    return response;
+  } catch (err) {
     throw new Error("Error in updating theatre");
   }
-}
+};
 
 const getTheatre = async (id) => {
   try {
@@ -70,71 +76,64 @@ const getTheatre = async (id) => {
   }
 };
 
-const getAllTheatres = async(data) => {
-    try {
-        let query = {};
-        let pagination = {};
-        if(data && data.city) {
-            query.city = data.city;
-        }
-        if(data && data.pincode) {
-            query.pincode = data.pincode;
-        }
-        if(data && data.name) {
-            query.name = data.name;
-        }
-        if(data && data.limit) {
-          pagination.limit = data.limit;
-        }
-        if(data && data.skip) {
-          let perPage = (data.limit) ? data.limit : 3
-          pagination.skip = data.skip * perPage;
-        }
-        const response = await Theatre.find(query, {}, pagination)
-        if(!response) {
-            return {
-                err: "Theatre not found",
-                code: 404
-            }
-        }
-        return response
-    } catch(err) {
-        throw new Error("Error in Fetching Theatres")
+const getAllTheatres = async (data) => {
+  try {
+    let query = {};
+    let pagination = {};
+    if (data && data.city) {
+      query.city = data.city;
     }
-}
-
-const updateMoviesInTheatres = async(theatreId, movieIds, insert) => {
-  const theatre = await Theatre.findById(theatreId)
-  if(!theatre) {
-    return {
-      err: "No such theatre found foe the id provided",
-      code: 404
+    if (data && data.pincode) {
+      query.pincode = data.pincode;
     }
+    if (data && data.name) {
+      query.name = data.name;
+    }
+    if (data && data.limit) {
+      pagination.limit = data.limit;
+    }
+    if (data && data.skip) {
+      let perPage = data.limit ? data.limit : 3;
+      pagination.skip = data.skip * perPage;
+    }
+    const response = await Theatre.find(query, {}, pagination);
+    if (!response) {
+      return {
+        err: "Theatre not found",
+        code: 404,
+      };
+    }
+    return response;
+  } catch (err) {
+    throw new Error("Error in Fetching Theatres");
   }
+};
 
-  if(insert) {
-    // we need to add movies
-    movieIds.forEach(movieId => {
-      theatre.movies.push(movieId)
-    })
-
-  } else {
-    // we need to remove movies
-    let savedMovieIds = theatre.movies;
-    movieIds.forEach(movieId => {
-      savedMovieIds = savedMovieIds.filter(smi => smi == movieId)
-      theatre.movies = savedMovieIds;
-    })
+const updateMoviesInTheatres = async (theatreId, movieIds, insert) => {
+  try {
+    if (insert) {
+      await Theatre.updateOne(
+        { _id: theatreId },
+        { $addToSet: { movies: { $each: movieIds } } },
+      );
+    } else {
+      await Theatre.updateOne(
+        { _id: theatreId },
+        { $pull: { movies: { $in: movieIds } } },
+      );
+    }
+    const theatre = await Theatre.findById(theatreId).populate("movies");
+    return theatre;
+  } catch (err) {
+    throw new Error("Error in updating movies in theatre");
   }
-  await theatre.save();
-  return theatre.populate('movies');
-}
+};
 
 module.exports = {
-    createTheatre,
-    deleteTheatre,
-    updateTheatre,
-    getTheatre,
-    getAllTheatres,
-    updateMoviesInTheatres
-}
+  createTheatre,
+  deleteTheatre,
+  updateTheatre,
+  getTheatre,
+  getAllTheatres,
+  updateMoviesInTheatres,
+};
