4f8ca6178aa39b2546149768fc9ca23394c7e500
Namballa-Srivansh <srivansh90@gmail.com>
2026-01-29 17:53:34 +0530

    Feat: Added validations for delete theatre api

--- Backend/routes/theatre.routes.js
+++ Backend/routes/theatre.routes.js
@@ -14,6 +14,7 @@ const routes = (app) => {
   app.delete(
     "/mba/api/v1/theatres/:id",
     authMiddleware.isAuthenticated,
+    authMiddleware.isAdminOrClient,
     theatreController.deleteTheatre,
   );
 
